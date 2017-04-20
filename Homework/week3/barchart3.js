438a924128d1f8df10a7a7fc6f4720ec26de5d40
Adriaandeklerk <ajhdeklerk@gmail.com>
2017-04-20 15:57:11 +0200

    Add files via upload

--- Homework/week3/barchart3.js
+++ Homework/week3/barchart3.js
@@ -3,8 +3,31 @@ var dataset = [3659, 10373, 13162, 5704];
 
 // width and height
 var svgWidth = 500;
-var svgHeight = 100;
+var svgHeight = 500;
 var barPadding = 1
+
+var xScale = d3.scale.linear()
+								 .domain([0, d3.max(dataset, function(d) { return d[0]; })])
+								 .range([barPadding, svgWidth - barPadding * 2]);
+
+var yScale = d3.scale.linear()
+					 .domain([0, d3.max(dataset, function(d) { return d[1]; })])
+					 .range([svgHeight - barPadding, barPadding]);
+
+var rScale = d3.scale.linear()
+					 .domain([0, d3.max(dataset, function(d) { return d[1]; })])
+					 .range([2, 5]);
+
+//Define X axis
+var xAxis = d3.svg.axis()
+				  .scale(xScale)
+				  .orient("bottom");
+
+//Create SVG element
+var svg = d3.select("body")
+			.append("svg")
+			.attr("width", svgWidth)
+			.attr("height", svgHeight);
 
 // create an svg element in page
 var svg = d3.select("body")
@@ -21,11 +44,14 @@ svg.selectAll("rect")
     return i * (svgWidth / dataset.length)
     })
    .attr("y", function(d) {
-    return svgHeight - d / 150;
+    return svgHeight - d / 50;
     })
    .attr("width", svgWidth / dataset.length - barPadding)
    .attr("height", function (d) {
      return d;
+    })
+    .attr("fill", function(d) {
+    return "rgb(0, 0, " + (d * 1) + ")";
     });
 
 // KLEUR TOEVOEGEN
@@ -38,8 +64,12 @@ svg.selectAll("text")
      return d;
    })
    .attr("x", function(d, i) {
-        return i * (svgWidth / dataset.length) + 40;
+        return i * (svgWidth / dataset.length) + 45;
    })
+   .attr("text-anchor", "middle")
    .attr("y", function(d) {
-        return svgHeight - (d / 150);
-   });
+        return svgHeight - (d / 60);
+   })
+   .attr("font-family", "sans-serif")
+   .attr("font-size", "11px")
+   .attr("fill", "white");
